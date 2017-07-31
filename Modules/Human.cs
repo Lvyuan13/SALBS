ee3ff35588b6b0d2bf8d095679e1edf944b338b9
HarryCLucas <harry.conor.lucas@gmail.com>
2017-07-31 17:27:33 +0930

    Fixed a bug in the human code and added heat to resource exchange

--- Modules/Human.cs
+++ Modules/Human.cs
@@ -38,14 +38,14 @@ namespace LunarParametricNumeric.Modules
             // TODO: Factor in varying breathing rates for intensities
             double airIntakeKG = 0.001225*airIntakeL; // There's 0.001225kg/L of air
 
-            if (airIntakeKG > 0){
+            if (airIntakeKG < 0){
                 double nitrogenIntake = 0.78*airIntakeKG;
                 double oxygenIntake = 0.21*airIntakeKG;
                 double co2Intake = 0.0004*airIntakeKG;
                 consumeResource(Resources.N, Convert.ToSingle(nitrogenIntake));
                 consumeResource(Resources.O, Convert.ToSingle(oxygenIntake));
                 consumeResource(Resources.CO2, Convert.ToSingle(co2Intake));
-            } else if (airIntakeKG < 0){
+            } else if (airIntakeKG > 0){
                 double nitrogenProduced = 0.78*airIntakeKG;
                 double oxygenProduced = 0.17*airIntakeKG;
                 double co2Produced = 0.05*airIntakeKG;
@@ -53,6 +53,9 @@ namespace LunarParametricNumeric.Modules
                 produceResource(Resources.O, Convert.ToSingle(oxygenProduced));
                 produceResource(Resources.CO2, Convert.ToSingle(co2Produced));
             }
+
+            double heatRelease = 118; // Humans release heat at 118W
+            produceResource(Resources.Enthalpy, Convert.ToSingle(heatRelease)); 
         }
         
     }
