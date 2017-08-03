66a46447f8f0674c439f5ef62680d94323c8ca97
lockieRichter <richter.lockie@gmail.com>
2017-08-04 08:50:21 +0930

    Added the basic implementation of the trasnport module

---
+++ Modules/Transport.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+namespace LunarParametricNumeric.Modules
+{
+	public class Transport: Module
+	{
+		/*	The LRV is just being monitered as a battery that
+			will be recharged whenever it is back at the base.
+		 */
+		private float batterySize; //[Ah]
+
+		/*	Figure out a way to model when the rover is driving,
+			possibly could be done using a probablity method, or
+			by simpling seeing what time of day it is.
+		 */
+		private boolean isDriving;
+
+		/*	Need to change this value into something meaningful. It's
+			meant to be the amount of Oxygen per second that a person
+			would consume whilst driving the LRV. At the moment it's 
+			just a dummy variable to allow the code to be written.
+		 */
+		private float humanOxygenIntake = 10.0f;
+
+		/*	Need to also change this value to the amount of enthalpy
+			produced by the rover and the humans when it is in 
+			operation. Currently just a dummmy variable.
+		 */
+		private float enthalpyProduced = 10.0f;
+
+		public Transport(Simulation sim, int moduleid) : base(sim,moduleid) {}
+
+		public override List<Resources> getResources() {
+			return new List<Resources>() {
+				Resources.O,
+				Resources.Enthalpy
+			};
+		}
+
+		public override string getModuleName(){
+            return "Transport";
+        }
+
+        public override string getModuleFriendlyName(){
+            return "Transport";
+        }
+
+        public override void setLoad(float value) {}
+
+        protected override void update(UInt64 clock) {
+        	if (isDriving) {
+        		consumeResource(Resources.O, Convert.ToSingle(humanOxygenIntake));
+        		produceResource(Resources.Enthalpy, Convert.ToSingle(enthalpyProduced));
+        	} else {
+        		/*	Do we need to put code in here to simulate the
+        			charging of the battery in the LRV. Something like
+        			if(batteryIsFlat) {
+						chargeBattery ()
+        			}
+        			?
+        		 */
+        	}
+        }
+
+        // Set the size of the battery in ampere hours
+		public void setBatterySize(float size) {
+			batterySize = size;
+		}
+	}
+}
