074f33f875b18b6ff189cc40364143d5486f30cd
lockieRichter <richter.lockie@gmail.com>
2017-08-29 14:53:00 +0930

    initial set up

---
+++ LunarNumericSimulator/Modules/Science.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+namespace LunarParametricNumeric.Modules
+{
+	public class Science: Module
+	{
+        private float energyUse;
+
+		public override List<Resources> getRegisteredResources(){
+            return new List<Resources>() { 
+                Resources.ElecticalEnergy;
+            };
+        } 
+
+        // This is just a dummy variable.
+        public override double getModuleVolume(){
+            return 10.0;
+        }
+
+        public override string moduleName {
+            get { return "ScienceModule"; }
+        }
+
+        public override string moduleFriendlyName
+        {
+            get { return "Science Module"; }
+        }
+
+        protected override void update(UInt64 clock) {
+            consumeResource(Resources.ElecticalEnergy, energyUse);
+        }
+
+        /*  Allow the user to set the amount of energy
+            useage required for the module as specified
+            in the prelim report. This will be the energy
+            used per day in kW and will then be converted
+            to kJ to be consumed every tick.
+         */
+        public void setUseage(float useage) {
+            energyUse = useage/(24 * 60 * 60)
+        }
+	}
+}
