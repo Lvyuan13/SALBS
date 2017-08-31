36a1327226c79914dae9724049d2cf4c2b16b399
HarryCLucas <harry.conor.lucas@gmail.com>
2017-08-31 12:19:56 +0930

    changes

--- LunarNumericSimulator/ResourceManagers/ThermodynamicEngine.cs
+++ LunarNumericSimulator/ResourceManagers/ThermodynamicEngine.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using LunarNumericSimulator.ResourceManagers;
-using System.Linq;
+using System;
+using System.Collections.Generic;
 using static LunarNumericSimulator.ResourceManagers.AtmosphericResourceManager;
 
 namespace LunarNumericSimulator
